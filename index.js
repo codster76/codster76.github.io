4e85b078d03fc557bb63878b4871b99d72bf8377
codster76 <codster761@gmail.com>
2022-08-02 17:08:09 +1000

    Turns out I forgot to enable that tsc command, so I wasn't modifying anything

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' })); // Only allow requests from localhost:3000
 app.use('/api/items', require('./routes/api/itemRoutes.js')); // /api/items is the url everything will be accessible from
-app.use(cors({ origin: 'localhost:3000/' }));
 app.get('/', (req, res) => {
     res.send('asdgasdg');
-});
-app.get('/poop', (req, res) => {
-    res.send('asdasfa');
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
