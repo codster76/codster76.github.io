7c765be0340a2f4ebb7a487ad5933fd8f677888e
codster76 <codster761@gmail.com>
2022-08-02 15:58:50 +1000

    Updated response header with CORS

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use('/api/items', require('./routes/api/itemRoutes.js')); // /api/items is the url everything will be accessible from
-app.use(cors());
+app.use(cors({ origin: 'https://hidden-atoll-35609.herokuapp.com/' }));
 app.get('/', (req, res) => {
     res.send('asdgasdg');
 });
 app.get('/poop', (req, res) => {
     res.send('asdasfa');
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Connected on port ${port}`);
 });
