6f574e8e6ed7082d2603e8121042f435db229502
codster76 <codster761@gmail.com>
2022-08-14 17:51:21 +1000

    Completed ascending item sorting

--- resume_site_frontend/src/components/HeadingAndSortingComponent.tsx
+++ resume_site_frontend/src/components/HeadingAndSortingComponent.tsx
@@ -8,14 +8,15 @@ export interface HeadingAndSortingComponentProps
     updateItems: Function
 }
 
-interface headingState
+interface HeadingState
 {
-    isString: boolean,
     currentHeading: string,
     updateHeading: Function,
     name: string,
-    headingType: itemType
+    sortFunction: (a:itemType, b:itemType) => number // It's optional because you only need to provide a comparison function for non-strings
 }
+
+let lastModified: HeadingState;
 
 const HeadingAndSortingComponent = (props: HeadingAndSortingComponentProps) =>
 {
@@ -25,73 +26,55 @@ const HeadingAndSortingComponent = (props: HeadingAndSortingComponentProps) =>
     const [valueHeading, updateValueHeading]: [string, Function] = useState('Value');
     const [weightHeading, updateWeightHeading]: [string, Function] = useState('Weight');
 
-    const stateList: headingState[] = [
-        {isString: true, currentHeading: nameHeading, updateHeading: updateNameHeading, name: 'Name'},
-        {isString: true, currentHeading: descriptionHeading, updateHeading: updateDescriptionHeading, name: 'Description'},
-        {isString: false, currentHeading: quantityHeading, updateHeading: updateQuantityHeading, name: 'Quantity'},
-        {isString: false, currentHeading: valueHeading, updateHeading: updateValueHeading, name: 'Value'},
-        {isString: false, currentHeading: weightHeading, updateHeading: updateWeightHeading, name: 'Weight'},
+    // This array is made so that all of the states can be iterated through to display
+    const stateList: HeadingState[] = [
+        {currentHeading: nameHeading, updateHeading: updateNameHeading, name: 'Name', sortFunction: (a, b) => a.name.localeCompare(b.name)},
+        {currentHeading: descriptionHeading, updateHeading: updateDescriptionHeading, name: 'Description', sortFunction: (a, b) => a.description.localeCompare(b.description)},
+        {currentHeading: quantityHeading, updateHeading: updateQuantityHeading, name: 'Quantity', sortFunction: (a, b) => a.quantity - b.quantity},
+        {currentHeading: valueHeading, updateHeading: updateValueHeading, name: 'Value', sortFunction: (a, b) => a.value - b.value},
+        {currentHeading: weightHeading, updateHeading: updateWeightHeading, name: 'Weight', sortFunction: (a, b) => a.weight - b.weight}
     ]
 
-    const sortItemArray = (isString: boolean, compareFunction?: (a:itemType, b:itemType) => number) => // '(a:itemType, b:itemType) => number' specifies the type of function.
+    // To ensure lastModified always has a value
+    if(lastModified === undefined)
     {
-        if(isString)
-        {
-            props.updateItems([...props.items.sort()]);
-        }
-        else
-        {
-            props.updateItems([...props.items.sort(compareFunction)]); // I don't 100% know why the spread operator is necessary, but I think updateItems needs a brand new array to actually render again
-        }
+        lastModified = stateList[0];
     }
 
-    const headingClickBehaviour = (states: headingState) =>
+    const headingClickBehaviour = (states: HeadingState) =>
     {
-        if(states.isString)
+        // Sort the array by the current heading
+        props.updateItems([...props.items.sort(states.sortFunction)]); // I don't 100% know why the spread operator is necessary, but I think updateItems needs a brand new array to actually render again
+
+        // Modify the heading names
+
+        // Basically, if the last heading modified was the one that was clicked, cycle through the sorting types
+        if(lastModified.name === states.name)
         {
-            sortItemArray(states.isString);
+            states.currentHeading === states.name ? states.updateHeading(states.name + '↓') : states.updateHeading(states.name);
+            lastModified.currentHeading = lastModified.name;
         }
         else
         {
-            sortItemArray(states.isString, (a:itemType, b:itemType) => a.value - b.value); // I just realised that my components don't know which itemType property to sort
+            lastModified.updateHeading(lastModified.name);
+            lastModified.currentHeading = lastModified.name;
+            states.currentHeading === states.name ? states.updateHeading(states.name + '↓') : states.updateHeading(states.name);
         }
-
-        // Toggle the ↓ in the heading whenever it's clicked
-        states.currentHeading === states.name ? states.updateHeading(states.name + '↓') : states.updateHeading(states.name);
+        lastModified = {...states};
     }
 
     return (
         <div>
             <div className = {style.flexContainer}>
                 {
-                    stateList.map((states) =>
+                    stateList.map((state) =>
                     (
-                        <div className = {style.flexItem} onClick={() => headingClickBehaviour(states)}>{states.currentHeading}</div>
+                        <div className = {style.flexItem} onClick={() => headingClickBehaviour(state)}>{state.currentHeading}</div>
                     ))
                 }
             </div>
         </div>
     );
 }
-
-/*
-<div className = {style.flexItem} onClick={() => {
-                    sortItemArray(true);
-                    nameHeading === 'Name' ? updateNameHeading('Name↓') : updateNameHeading('Name');
-                    }}>
-                    {nameHeading}
-                </div>
-
-                <div className = {style.flexItem} onClick={() => {
-                    sortItemArray(true, (a:itemType, b:itemType) => a.value - b.value);
-                    descriptionHeading === 'Description' ? updatedescriptionHeading('Description↓') : updateNameHeading('Name')
-                    }}>
-                    {descriptionHeading}
-                </div>
-
-                <div className = {style.flexItem} onClick={() => sortItemArray(false, (a:itemType, b:itemType) => a.quantity - b.quantity)}>{sortBy === 'Quantity' ? 'Quantity↓' : 'Quantity'}</div>
-                <div className = {style.flexItem} onClick={() => sortItemArray(false, (a:itemType, b:itemType) => a.value - b.value)}>{sortBy === 'Value' ? 'Value↓' : 'Value'}</div>
-                <div className = {style.flexItem} onClick={() => sortItemArray(false, (a:itemType, b:itemType) => a.weight - b.weight)}>{sortBy === 'Weight' ? 'Weight↓' : 'Weight'}</div>
-*/
 
 export default HeadingAndSortingComponent;
